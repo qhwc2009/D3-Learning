05e7ec835e5f03b46a67c28eb93ff2e6aa26d358
wangchen <wang.chen@mioying.com>
2019-12-16 18:24:00 +0800

    init project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link, Redirect } from 'react-router-dom';
 
-function App() {
+import { Layout, Menu } from 'antd';
+
+import Home from 'view/Home/Home';
+import css from './App.module.scss';
+
+const { Header, Footer, Sider, Content } = Layout;
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className={css.app}>
+        <Layout className={css.container}>
+          <Header>
+            <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']} style={{ lineHeight: '64px' }}>
+              <Menu.Item key="1">nav 1</Menu.Item>
+              <Menu.Item key="2">nav 2</Menu.Item>
+              <Menu.Item key="3">nav 3</Menu.Item>
+            </Menu>
+          </Header>
+          <Layout className={css.body}>
+            <Sider>
+              <Menu className={css.sideBarMenu} theme="light" mode="inline" defaultSelectedKeys={['1']}>
+                <Menu.Item key="1">
+                  <Link className="nav-text" to="/home">
+                    home
+                  </Link>
+                </Menu.Item>
+              </Menu>
+            </Sider>
+            <Content>
+              <Switch>
+                <Route path="/home">
+                  <Home />
+                </Route>
+                <Route exact path="/">
+                  <Redirect to="/home" />
+                </Route>
+              </Switch>
+            </Content>
+          </Layout>
+          <Footer className={css.footer}>Footer</Footer>
+        </Layout>
+      </div>
+    </Router>
   );
 }
-
-export default App;
---
+++ src/view/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import css from './Home.module.scss';
+
+function Home() {
+  return (
+    <div className={css.home}>
+      <div className={css.content}>
+        这是D3工程
+      </div>
+    </div>
+  );
+}
+
+export default Home;
